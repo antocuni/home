63f91b62c423a20e1a1a006860477d6d3790e9c5
Antonio Cuni <anto.cuni@gmail.com>
2008-11-11 15:26:25 +0000

    various improvements to my scripts

--- bin/blamepipe.py
+++ bin/blamepipe.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import re
 import sys
 import os.path
 import rxvtlib
@@ -8,24 +9,20 @@ from color import color, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, GRAY
 
 COLORS = [CYAN, RED, GREEN, YELLOW, BLUE, MAGENTA, GRAY]
 
+regexp = re.compile(r'( *[0-9]+)( *[^ ]+)(.*)')
 def parseline(line):
-    line = line.lstrip()
-    rev, line = line.split(' ', 1)
-    int(rev) # may raise ValueError
-    line = line.lstrip()
-    if ' ' not in line:
-        author = line
-        return rev, author, ''
-    author, line = line.split(' ', 1)
-    return rev, author, line
+    match = regexp.match(line)
+    if not match:
+        raise ValueError
+    return match.groups()
 
 def test_parseline():
     import py
-    assert parseline(' 12345   antocuni bla bla') == ('12345', 'antocuni', 'bla bla')
-    assert parseline(' 12345   antocuni     bla') == ('12345', 'antocuni', '    bla')
-    assert parseline(' 12345   antocuni')         == ('12345', 'antocuni', '')
-    assert parseline(' 12345   antocuni  ')        == ('12345', 'antocuni', ' ')
-    assert parseline('12345    antocuni bla bla')  == ('12345', 'antocuni', 'bla bla')
+    assert parseline(' 12345   antocuni bla bla') == (' 12345', '   antocuni', ' bla bla')
+    assert parseline(' 12345   antocuni     bla') == (' 12345', '   antocuni', '     bla')
+    assert parseline(' 12345   antocuni')         == (' 12345', '   antocuni', '')
+    assert parseline(' 12345   antocuni  ')       == (' 12345', '   antocuni', '  ')
+    assert parseline('123456       anto bla bla') == ('123456', '       anto', ' bla bla')
     py.test.raises(ValueError, parseline, ' xxx   antocuni bla bla')
 
 def cycle(key, key2color, bg):
@@ -55,7 +52,7 @@ def main():
         rev = cycle(rev, rev2color, bg=1)
         rev = rxvtlib.format_command(rev, cmd, force=True)
         author = cycle(author, author2color, bg=0)
-        sys.stdout.write(' %s   %s %s' % (rev, author, text))
+        sys.stdout.write('%s%s%s\n' % (rev, author, text))
         
 
 if __name__ == '__main__':
