a4df51f2c4e72736012165949be9d9cd3ada1c94
Carl Friedrich Bolz <cfbolz@gmx.de>
2010-07-15 14:19:34 +0000

    add possibility to do syntax highlighting in slides, like this:
    .. sourcecode:: python
    
        code goes here

--- bin/rst2beamer.py
+++ bin/rst2beamer.py
@@ -7,7 +7,7 @@ Beamer is a LaTeX document class for presentations. Via this script, ReST can
 be used to prepare slides. It can be called::
 
         rst2beamer.py infile.txt > outfile.tex
-        
+
 where ``infile.tex`` contains the produced Beamer LaTeX.
 
 See <http:www.agapow.net/programming/python/rst2beamer> for more details.
@@ -35,6 +35,50 @@ from docutils.core import publish_cmdline, default_description
 from docutils.writers.latex2e import Writer as Latex2eWriter
 from docutils.writers.latex2e import LaTeXTranslator, DocumentClass
 from docutils import nodes
+
+## Syntax highlighting:
+
+"""
+        .. sourcecode:: python
+
+            My code goes here.
+
+
+    :copyright: 2007 by Georg Brandl.
+    :license: BSD, see LICENSE for more details.
+"""
+
+from pygments.formatters import HtmlFormatter, LatexFormatter
+
+# The default formatter
+DEFAULT = LatexFormatter()
+
+
+from docutils.parsers.rst import directives
+
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name, TextLexer
+
+VARIANTS = {
+    'linenos': LatexFormatter(linenos=True),
+}
+
+def pygments_directive(name, arguments, options, content, lineno,
+                       content_offset, block_text, state, state_machine):
+    try:
+        lexer = get_lexer_by_name(arguments[0])
+    except ValueError:
+        # no lexer found - use the text one instead of an exception
+        lexer = TextLexer()
+    formatter = DEFAULT
+    parsed = highlight(u'\n'.join(content), lexer, formatter)
+    return [nodes.raw('', parsed, format='latex')]
+
+pygments_directive.arguments = (1, 0, 1)
+pygments_directive.content = 1
+pygments_directive.options = dict([(key, directives.flag) for key in VARIANTS])
+
+directives.register_directive('sourcecode', pygments_directive)
 
 ## CONSTANTS & DEFINES: ###
 
@@ -80,8 +124,14 @@ class BeamerTranslator (LaTeXTranslator):
                 LaTeXTranslator.__init__ (self, document)
                 self.head_prefix = [x for x in self.head_prefix if ('{typearea}' not in x)]
                 hyperref_posn = [i for i in range (len (self.head_prefix)) if ('{hyperref}' in self.head_prefix[i])]
-                if hyperref_posn:
-                        self.head_prefix[hyperref_posn[0]] = '\\usepackage{hyperref}\n'
+                if not hyperref_posn:
+                        self.head_prefix.append(None)
+                        hyperref_posn = [-1] # XXX hack
+                self.head_prefix[hyperref_posn[0]] = ('\\usepackage{hyperref}\n' +
+                                                      '\\usepackage{fancyvrb}\n' +
+                                                      LatexFormatter(style="manni").get_style_defs() +
+                                                      "\n")
+
                 self.head_prefix.extend ([
                         '\\definecolor{rrblitbackground}{rgb}{0.55, 0.3, 0.1}\n',
                         '\\newenvironment{rtbliteral}{\n',
@@ -94,15 +144,18 @@ class BeamerTranslator (LaTeXTranslator):
                 # this fixes the hardcoded section titles in docutils 0.4
                 self.d_class = DocumentClass ('article')
 
-        def begin_frametag (self):
-                return '\\begin{frame}\n'
+        def begin_frametag (self, node):
+                if "verbatim" in str(node).lower():
+                    return '\\begin{frame}[containsverbatim]\n'
+                else:
+                    return '\\begin{frame}\n'
 
         def end_frametag (self):
                 return '\\end{frame}\n'
 
         def visit_section (self, node):
                 if (self.section_level == 0):
-                        self.body.append (self.begin_frametag())
+                        self.body.append (self.begin_frametag(node))
                 LaTeXTranslator.visit_section (self, node)
 
         def depart_section (self, node):
@@ -159,6 +212,8 @@ class BeamerWriter (Latex2eWriter):
         def __init__(self):
                 Latex2eWriter.__init__(self)
                 self.translator_class = BeamerTranslator
+
+
 
 
 if __name__ == '__main__':
