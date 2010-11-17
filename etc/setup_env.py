81408e9e72bf7c6cc6ad301067cc8f6c1cdf38bd
Antonio Cuni <anto.cuni@gmail.com>
2010-11-18 00:18:34 +0100

    after creating symlinks, call 'setup.py' develop on all the projects found in src/

--- etc/setup_env.py
+++ etc/setup_env.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 import os.path
+import glob
 
 home = os.path.expanduser('~')
-path = os.path.dirname(os.path.abspath(__file__))
+etc_dir = os.path.dirname(os.path.abspath(__file__))
 excludes = ['create_symlinks.py', 'scripts', 'elisp']
 
 def color(s, fg=1, bg=1):
@@ -16,7 +17,8 @@ def symlink(src, dst):
         link = os.readlink(dst)
         if link == src:
             return # nothing to do
-        if link.startswith('/home/antocuni/pypy/user/antocuni/'):
+        if link.startswith('/home/antocuni/pypy/user/antocuni/') or\
+           link.startswith('pypy/user/antocuni'):
             # old location, kill it
             os.remove(dst)
     except OSError:
@@ -25,15 +27,16 @@ def symlink(src, dst):
 
 def do_symlink(src, dst):
     try:
-        print '%s -> %s' % (src.replace(path, '.'), dst.replace(home, '~')),
+        print '%s -> %s' % (src.replace(etc_dir, '.'), dst.replace(home, '~')),
         symlink(src, dst)
         print
     except Exception, msg:
         print color("Failed: %s" % (msg,), 31)
 
 
-def main():
-    for f in os.listdir(path):
+def create_symlinks():
+    print color('Creating symlinks', 33)
+    for f in os.listdir(etc_dir):
         if (f.startswith('.') or
             f.endswith('~') or
             f.endswith('.pyc') or
@@ -45,11 +48,26 @@ def main():
 
     more_links = [
         ('~/env/src/pdb/pdbrc.py', '~/.pdbrc.py'),
+        ('~/env/bin', '~/bin'),
+        ('~/env/src', '~/src'),
         ]
     for src, dst in more_links:
         src = os.path.expanduser(src)
         dst = os.path.expanduser(dst)
         do_symlink(src, dst)
 
+def install_py_packages():
+    orig_dir = os.getcwd()
+    envdir = os.path.dirname(etc_dir)
+    pattern = os.path.join(envdir, 'src', '*', 'setup.py')
+    for setup_py in glob.glob(pattern):
+        print
+        print color('Installing %s' % setup_py, 33)
+        path = os.path.dirname(setup_py)
+        os.chdir(path)
+        os.system('python "%s" develop --user' % setup_py)
+    os.chdir(orig_dir)
+
 if __name__ == '__main__':
-    main()
+    create_symlinks()
+    install_py_packages()
