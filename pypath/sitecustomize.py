c202169107c5d81ac8b388309e5a58431293306e
Antonio Cuni <anto.cuni@gmail.com>
2010-12-14 16:36:13 +0100

    ignore bdbquit

--- pypath/sitecustomize.py
+++ pypath/sitecustomize.py
@@ -1,11 +1,12 @@
 import sys
+import bdb
 
 def isatty(stream):
     return hasattr(stream, 'isatty') and stream.isatty()
 
 def info(type, value, tb):
     ontty = isatty(sys.stderr) and isatty(sys.stdout) and isatty(sys.stdin)
-    if hasattr(sys, 'ps1') or not ontty or\
+    if hasattr(sys, 'ps1a') or not ontty or type is bdb.BdbQuit or \
             str(value) == 'underlying C/C++ object has been deleted':
         # You are in interactive mode or don't have a tty-like
         # device, so call the default hook
