ed50ba88f54b33d0f391ebcaf6fbbd761b209d09
Antonio Cuni <anto.cuni@gmail.com>
2014-07-15 11:20:07 +0200

    start gmail instead of thunderbird

--- bin/start-thunderbird.py
+++ bin/start-thunderbird.py
@@ -5,16 +5,18 @@ import time
 import wmctrl
 
 def main():
-    os.system('LC_ALL=en_IE.utf8 thunderbird &')
+    os.system('chromium-browser --app=http://gmail.com')
+    
+    ## os.system('LC_ALL=en_IE.utf8 thunderbird &')
 
-    while True:
-        windows = wmctrl.Window.by_name_endswith('Thunderbird')
-        if not windows:
-            time.sleep(0.5)
-            continue
-        for win in windows:
-            win.set_geometry(os.environ['THUNDERBIRD_GEOMETRY'])
-        break
+    ## while True:
+    ##     windows = wmctrl.Window.by_name_endswith('Thunderbird')
+    ##     if not windows:
+    ##         time.sleep(0.5)
+    ##         continue
+    ##     for win in windows:
+    ##         win.set_geometry(os.environ['THUNDERBIRD_GEOMETRY'])
+    ##     break
 
 if __name__ == '__main__':
     main()
