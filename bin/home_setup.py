3f54307425a46d18f61f54ac517c3fef8014e1e1
Antonio Cuni <anto.cuni@gmail.com>
2015-11-30 19:00:19 +0000

    ubuntu does not source ~/.bash_profile, but ~/.profile; link both

--- bin/home_setup.py
+++ bin/home_setup.py
@@ -116,6 +116,7 @@ def create_symlinks():
         ('~/env/src/pdb/pdbrc.py', '~/.pdbrc.py'),
         ('~/env/bin', '~/bin'),
         ('~/env/etc/gtk-3.0/gtk.css', '~/.config/gtk-3.0/gtk.css'),
+        ('~/env/etc/bash_profile', '~/.profile'),
         ]
     for src, dst in more_links:
         src = os.path.expanduser(src)
