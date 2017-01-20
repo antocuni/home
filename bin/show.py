62fc302f132d148c6949c959f494ff76fc507c02
Antonio Cuni <anto.cuni@gmail.com>
2017-01-20 11:33:22 +0100

    experiment with show.py, but then don't use it

--- bin/show.py
+++ bin/show.py
@@ -10,15 +10,37 @@ else:
     print 'Usage: show.py [term|emacs]'
     sys.exit(1)
 
+def show_emacs_and_term(arg):
+    emacs = wmctrl.Window.by_class('emacs.Emacs24')
+    if emacs:
+        emacs = emacs[0]
+    #
+    term = wmctrl.Window.by_role("autoterm")
+    if term:
+        term = term[0]
+    #
+    H = 2160
+    if arg == 'emacs':
+        emacs.resize_and_move(y=0, h=1680)
+        #term.resize_and_move(y=1680, h=H-1680)
+        return emacs
+    else:
+        term.resize_and_move(y=480, h=H-480)
+        #emacs.resize_and_move(y=0, h=480)
+        return term
+
+
 wlist = []
 if arg == 'term':
-    #wlist = wmctrl.Window.by_role("autoterm")
+    ## wlist = wmctrl.Window.by_role("autoterm")
     os.system('wmctrl -x -a gnome-terminal')
 elif arg == 'emacs':
     os.system('wmctrl -x -a emacs')
     sys.exit(0)
     ## wlist = wmctrl.Window.by_class('emacs.Emacs24')
     ## wlist += wmctrl.Window.by_class('emacs24.Emacs24')
+## if arg in ('term', 'emacs'):
+##     wlist = [show_emacs_and_term(arg)]
 elif arg == 'xchat':
     wlist = wmctrl.Window.by_class('hexchat.Hexchat')
     wlist.sort(key=lambda w: w.wm_name)
