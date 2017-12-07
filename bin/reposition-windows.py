4e4006716ef6233c6b1e438c5b0732b4130cfb26
Antonio Cuni <anto.cuni@gmail.com>
2017-12-07 23:54:52 +0100

    add again a script to position the windows as I like

---
+++ bin/reposition-windows.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import os
+from wmctrl import Window
+
+def main():
+    # position windows on my 4k screen
+
+    for win in Window.by_class('emacs.Emacs'):
+        win.resize_and_move(x=1804, y=0, w=2036, h=1650)
+
+    for win in Window.by_role('autoterm'):
+        win.resize_and_move(x=1811, y=755, w=2029, h=1405)
+
+    for win in Window.by_class('mail.google.com.Google-chrome'):
+        win.resize_and_move(x=73, y=0, w=1728, h=1803)
+
+if __name__ == '__main__':
+    main()
