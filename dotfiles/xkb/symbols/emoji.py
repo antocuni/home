15130974076f87d0cdd0b770aee6573c2cbacf69
Antonio Cuni <anto.cuni@gmail.com>
2018-05-31 11:02:58 +0200

    add one symbol

--- dotfiles/xkb/symbols/emoji.py
+++ dotfiles/xkb/symbols/emoji.py
@@ -2,7 +2,7 @@
 # -*- encoding: utf-8 -*-
 import unicodedata
 
-emojis = u'😂😊❤😘😱👍👏⛄😴🎉😛😋😍😞😁😡😅🔝😳'
+emojis = u'😂😊❤😘😱👍👏⛄😴🎉😛😋😍😞😁😡😅🔝😳😭😤'
 
 for char in emojis:
     print('#  ', char, '   U%05X' % ord(char), unicodedata.name(char, '...'))
